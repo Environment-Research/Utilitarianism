c23b45ed655dc5a7cf61c66afff12a974bf3547c
Frank <frankerrickson@berkeley.edu>
2019-09-18 21:08:46 -0700

    Edit modified welfare component
    In particular, it needs to have the same variable name for total welfare ("UTILITY") or else it gets tricky to swithc Negishi weights on and off.

--- src/new_components/updated_welfare.jl
+++ src/new_components/updated_welfare.jl
@@ -2,7 +2,7 @@
 # Total Welfare
 # -----------------------------------------------
 
-@defcomp updated_welfare begin
+@defcomp welfare begin
 
     regions  = Index()                         # 12 regions in RICE2010.
 
@@ -11,23 +11,23 @@
     pop     = Parameter(index=[time, regions]) # Regional population levels (millions of people).
     cpc     = Parameter(index=[time, regions]) # Per capita consumption levels ($1000s).
 
-    welfare = Variable()           # Total economic welfare over model time horizon.
+    UTILITY = Variable()                       # Total economic welfare over model time horizon (using the same name as the default RICE welfare component).
 
 
     function run_timestep(p, v, d, t)
 
-        # Calculate welfare for each period. Note, if η = 1, the social welfare function becomes log(x).
+        # Calculate total welfare for each period. Note, if η = 1, the social welfare function becomes log(x).
         if is_first(t)
             if p.η == 1.0
-                v.welfare = sum(log(p.cpc[t,:]) .* p.pop[t,:]) / (1.0 + p.ρ)^(10*(t.t - 1))
+                v.UTILITY = sum(log(p.cpc[t,:]) .* p.pop[t,:]) / (1.0 + p.ρ)^(10*(t.t - 1))
             else
-                v.welfare = sum((p.cpc[t,:] .^ (1.0 - p.η)) ./ (1.0 - p.η) .* p.pop[t,:]) / (1.0 + p.ρ)^(10*(t.t - 1))
+                v.UTILITY = sum((p.cpc[t,:] .^ (1.0 - p.η)) ./ (1.0 - p.η) .* p.pop[t,:]) / (1.0 + p.ρ)^(10*(t.t - 1))
             end
         else
             if p.η == 1.0
-                v.welfare = v.welfare + sum(log(p.cpc[t,:]) .* p.pop[t,:]) / (1.0 + p.ρ)^(10*(t.t - 1))
+                v.UTILITY = v.UTILITY + sum(log(p.cpc[t,:]) .* p.pop[t,:]) / (1.0 + p.ρ)^(10*(t.t - 1))
             else
-                v.welfare = v.welfare + sum((p.cpc[t,:] .^ (1.0 - p.η)) ./ (1.0 - p.η) .* p.pop[t,:]) / (1.0 + p.ρ)^(10*(t.t - 1))
+                v.UTILITY = v.UTILITY + sum((p.cpc[t,:] .^ (1.0 - p.η)) ./ (1.0 - p.η) .* p.pop[t,:]) / (1.0 + p.ρ)^(10*(t.t - 1))
             end
         end
     end
