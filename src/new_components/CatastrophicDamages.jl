595c4bd04bb0cd117fd64663cd9f12fe22529667
kevinkuruc <kkuruc@ou.edu>
2020-01-18 18:16:41 -0600

    Add files via upload

---
+++ src/new_components/CatastrophicDamages.jl
@@ -0,0 +1,30 @@
+@defcomp damages begin
+    regions = Index()
+
+    DAMFRAC = Variable(index=[time, regions]) # Damages as % of GDP
+    DAMAGES = Variable(index=[time, regions]) # Damages (trillions 2005 USD per year)
+
+    TATM = Parameter(index=[time]) # Increase temperature of atmosphere (degrees C from 1900)
+    YGROSS = Parameter(index=[time, regions]) # Gross world product GROSS of abatement and damages (trillions 2005 USD per year)
+    SLRDAMAGES = Parameter(index=[time, regions])
+    a1 = Parameter(index=[regions]) # Damage intercept
+    a2 = Parameter(index=[regions]) # Damage quadratic term
+    a3 = Parameter(index=[regions]) # Damage exponent
+
+    function run_timestep(p, v, d, t)
+
+        #Define function for DAMFRAC
+        for r in d.regions
+            v.DAMFRAC[t,r] = (((p.a1[r] * p.TATM[t]) + (p.a2[r] * p.TATM[t]^p.a3[r]) + .000153*p.TATM[t]^7) / 100) + (p.SLRDAMAGES[t,r] / 100)
+        end
+
+        #Define function for DAMAGES
+        for r in d.regions
+            if is_first(t)
+                v.DAMAGES[t,r] = p.YGROSS[t,r] * (1 - 1 / (1+v.DAMFRAC[t,r]))
+            else
+                v.DAMAGES[t,r] = (p.YGROSS[t,r] * v.DAMFRAC[t,r]) / (1. + v.DAMFRAC[t,r]^10)
+            end
+        end
+    end
+end
---
+++ src/new_components/Emissions_SlowDown_rice.jl
@@ -0,0 +1,56 @@
+@defcomp emissions begin
+    regions = Index()
+
+    E = Variable(index=[time]) # Total CO2 emissions (GtC per year)
+    EIND = Variable(index=[time, regions]) # Industrial emissions (GtC per year)
+    CCA = Variable(index=[time]) # Cumulative indiustrial emissions
+    ABATECOST = Variable(index=[time, regions]) # Cost of emissions reductions  (trillions 2005 USD per year)
+    MCABATE = Variable(index=[time, regions]) # Marginal cost of abatement (2005$ per ton CO2)
+    CPRICE = Variable(index=[time, regions]) # Carbon price (2005$ per ton of CO2)
+
+    sigma = Parameter(index=[time, regions]) # CO2-equivalent-emissions output ratio
+    YGROSS = Parameter(index=[time, regions]) # Gross world product GROSS of abatement and damages (trillions 2005 USD per year)
+    etree = Parameter(index=[time]) # Emissions from deforestation
+    cost1 = Parameter(index=[time, regions]) # Adjusted cost for backstop
+    expcost2 = Parameter(index=[regions]) # Exponent of control cost function
+    partfract = Parameter(index=[time, regions]) # Fraction of emissions in control regime
+    pbacktime = Parameter(index=[time, regions]) # Backstop price
+    MIU = Parameter(index=[time, regions]) # Emission control rate GHGs
+
+    function run_timestep(p, v, d, t)
+
+        #Define function for EIND
+        for r in d.regions
+            v.EIND[t,r] = p.sigma[t,r] * p.YGROSS[t,r] * (1-p.MIU[t,r])
+        end
+
+        #Define function for E
+        v.E[t] = sum(v.EIND[t,:]) + p.etree[t]
+
+        #Define function for CCA
+        if is_first(t)
+            v.CCA[t] = sum(v.EIND[t,:]) * 10.
+        else
+            v.CCA[t] =  v.CCA[t-1] + (sum(v.EIND[t,:]) * 10.)
+        end
+
+        #Define function for ABATECOST
+        for r in d.regions
+        if t.t==1
+            v.ABATECOST[t,r] = p.YGROSS[t,r] * p.cost1[t,r] * (p.MIU[t,r]^p.expcost2[r]) * (p.partfract[t,r]^(1 - p.expcost2[r]))
+        else
+            v.ABATECOST[t,r] = p.YGROSS[t,r] * p.cost1[t,r] * (p.MIU[t,r]^p.expcost2[r] + 80*(p.MIU[t,r] - p.MIU[t-1,r])^4) * (p.partfract[t,r]^(1 - p.expcost2[r]))
+        end
+
+        #Define function for MCABATE
+        for r in d.regions
+            v.MCABATE[t,r] = p.pbacktime[t,r] * p.MIU[t,r]^(p.expcost2[r] - 1)
+        end
+
+        #Define function for CPRICE
+        for r in d.regions
+            v.CPRICE[t,r] = p.pbacktime[t,r] * 1000 * p.MIU[t,r]^(p.expcost2[r] - 1)
+        end
+    end
+end
+end
